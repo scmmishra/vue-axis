c62842a7b82b062d2a8e390e05e9f2caaf19693e
Shivam Mishra <scm.mymail@gmail.com>
2024-10-01 13:17:15 +0530

    feat: allow toggling data

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,13 +1,19 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { AxisChart } from "../../src";
 import { generateNumbers, generateDates } from "./utils/generator";
 
-const dataCount = 15;
+const stacked = ref(true);
+const spaceRatio = ref(0.2);
+const xOffset = ref(20);
+const yOffset = ref(20);
+const dataCount = ref(15);
 
-const openConvCount = generateNumbers(dataCount, 1, 100);
-const resolvedConvCount = generateNumbers(dataCount, 1, 100);
-const labels = generateDates(dataCount, "day");
+const openConvCount = computed(() => generateNumbers(dataCount.value, 1, 100));
+const resolvedConvCount = computed(() =>
+  generateNumbers(dataCount.value, 1, 100),
+);
+const labels = computed(() => generateDates(dataCount.value, "day"));
 
 const formatXAxis = (value: string | number | Date) => {
   return new Date(value).getFullYear();
@@ -17,28 +23,24 @@ const formatYAxis = (value: number) => {
   return `${value}%`;
 };
 
-const stacked = ref(true);
-const spaceRatio = ref(0.2);
-const xOffset = ref(20);
-const yOffset = ref(20);
-
-const dataset = [
+const dataset = computed(() => [
   {
-    values: openConvCount,
+    values: openConvCount.value,
     color: "#064e3b",
     name: "Conversations open",
     type: "bar",
   },
   {
-    values: resolvedConvCount,
+    values: resolvedConvCount.value,
     color: "#4ade80",
     name: "Conversations resolved",
     type: "bar",
   },
-];
+]);
 </script>
 
 <template>
+  {{ dataset }}
   <section class="max-w-5xl mx-auto py-12">
     <h2 class="text-2xl font-bold mb-5">Stacked Bar Chart</h2>
 
@@ -50,6 +52,10 @@ const dataset = [
       <label class="grid">
         Space Ratio
         <input type="range" min="0" max="1" step="0.1" v-model="spaceRatio" />
+      </label>
+      <label class="grid">
+        Number of data points
+        <input type="range" min="5" max="50" step="0.1" v-model="dataCount" />
       </label>
     </div>
     <div
