0faa7e0ab3f18c2ad1bd076d99c4763452e6489e
Shivam Mishra <scm.mymail@gmail.com>
2024-10-01 12:16:40 +0530

    feat: setup demo

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { ref } from "vue";
 import { AxisChart } from "../../src";
 import { generateNumbers, generateDates } from "./utils/generator";
 
@@ -15,6 +16,8 @@ const formatXAxis = (value: string | number | Date) => {
 const formatYAxis = (value: number) => {
   return `${value}%`;
 };
+
+const stacked = ref(true);
 
 const dataset = [
   {
@@ -33,14 +36,32 @@ const dataset = [
 </script>
 
 <template>
-  <div>
-    <AxisChart
-      :max-width="900"
-      stacked
-      :labels="labels"
-      :dataset="dataset"
-      :format-y="formatYAxis"
-      :format-x="formatXAxis"
-    />
-  </div>
+  <section class="max-w-5xl mx-auto py-12">
+    <h2 class="text-2xl font-bold mb-5">Stacked Bar Chart</h2>
+    <div>
+      <span class="text-gray-600">Controls</span>
+      <div>
+        <label>
+          <input type="checkbox" v-model="stacked" />
+
+          Stacked
+        </label>
+      </div>
+    </div>
+    <div
+      class="p-5 shadow-sm outline outline-1 outline-gray-200 rounded-md mx-auto mt-5"
+    >
+      <div class="w-full">
+        <AxisChart
+          :max-width="1300"
+          :height="400"
+          :stacked="stacked"
+          :labels="labels"
+          :dataset="dataset"
+          :format-y="formatYAxis"
+          :format-x="formatXAxis"
+        />
+      </div>
+    </div>
+  </section>
 </template>
