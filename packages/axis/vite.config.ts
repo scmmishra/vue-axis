a078bc7198c5fc7172632df90352f9c8c69b4116
Shivam Mishra <scm.mymail@gmail.com>
2025-01-16 12:19:33 +0530

    feat: setup typescript

--- packages/axis/vite.config.ts
+++ packages/axis/vite.config.ts
@@ -1,9 +1,13 @@
 import { resolve } from "node:path";
 import Vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import Dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [Vue()],
+  plugins: [
+    Vue(),
+    Dts({ tsconfigPath: resolve(__dirname, "tsconfig.app.json") }),
+  ],
   build: {
     lib: {
       formats: ["es"],
